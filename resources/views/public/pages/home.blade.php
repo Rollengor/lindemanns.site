63c95f2811749b856303b967d66d1a61f0f4dd7d
Alexander Silka <alexander.silka@gmail.com>
2025-12-16 09:32:20 +0200

    new pages

--- resources/views/public/pages/home.blade.php
+++ resources/views/public/pages/home.blade.php
@@ -21,13 +21,13 @@
                 <div class="services-row-body">
                     <h3 class="services-row-title">Real Estate Services</h3>
                     <ul>
-                        <li><a href="#" class="primary-link">Searching and brokerage of real estate objects in Switzerland</a></li>
-                        <li><a href="#" class="primary-link">Planning of real estate</a></li>
-                        <li><a href="#" class="primary-link">Development of real estate</a></li>
-                        <li><a href="#" class="primary-link">Marketing of real estate</a></li>
-                        <li><a href="#" class="primary-link">Brokerage</a></li>
-                        <li><a href="#" class="primary-link">Property management</a></li>
-                        <li><a href="#" class="primary-link">Renovation</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Searching and brokerage of real estate objects in Switzerland</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Planning of real estate</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Development of real estate</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Marketing of real estate</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Brokerage</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Property management</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Renovation</a></li>
                     </ul>
                 </div>
                 {{--<div class="services-row-pictures">
@@ -41,10 +41,10 @@
                 <div class="services-row-body">
                     <h3 class="services-row-title">Financial & Investment Advisory</h3>
                     <ul>
-                        <li><a href="#" class="primary-link">Corporate finance advisory for real estate projects</a></li>
-                        <li><a href="#" class="primary-link">Investment consulting</a></li>
-                        <li><a href="#" class="primary-link">Tax</a></li>
-                        <li><a href="#" class="primary-link">Wealth protection</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Corporate finance advisory for real estate projects</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Investment consulting</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Tax</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Wealth protection</a></li>
                     </ul>
                 </div>
                 <img src="/img/temp/service-2.webp?v-2" alt="Service image" class="img services-row-image">
@@ -54,8 +54,8 @@
                 <div class="services-row-body">
                     <h3 class="services-row-title">Legal & Relocation Services</h3>
                     <ul>
-                        <li><a href="#" class="primary-link">Legal structuring</a></li>
-                        <li><a href="#" class="primary-link">Relocation to Switzerland</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Legal structuring</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Relocation to Switzerland</a></li>
                     </ul>
                 </div>
                 <img src="/img/temp/service-3.webp?v-2" alt="Service image" class="img services-row-image">
--- resources/views/public/pages/services/page.blade.php
+++ resources/views/public/pages/services/page.blade.php
@@ -11,13 +11,13 @@
                 <div class="services-row-body">
                     <h3 class="services-row-title">Real Estate Services</h3>
                     <ul>
-                        <li><a href="#" class="primary-link">Searching and brokerage of real estate objects in Switzerland</a></li>
-                        <li><a href="#" class="primary-link">Planning of real estate</a></li>
-                        <li><a href="#" class="primary-link">Development of real estate</a></li>
-                        <li><a href="#" class="primary-link">Marketing of real estate</a></li>
-                        <li><a href="#" class="primary-link">Brokerage</a></li>
-                        <li><a href="#" class="primary-link">Property management</a></li>
-                        <li><a href="#" class="primary-link">Renovation</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Searching and brokerage of real estate objects in Switzerland</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Planning of real estate</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Development of real estate</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Marketing of real estate</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Brokerage</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Property management</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Renovation</a></li>
                     </ul>
                 </div>
                 {{--<div class="services-row-pictures">
@@ -31,10 +31,10 @@
                 <div class="services-row-body">
                     <h3 class="services-row-title">Financial & Investment Advisory</h3>
                     <ul>
-                        <li><a href="#" class="primary-link">Corporate finance advisory for real estate projects</a></li>
-                        <li><a href="#" class="primary-link">Investment consulting</a></li>
-                        <li><a href="#" class="primary-link">Tax</a></li>
-                        <li><a href="#" class="primary-link">Wealth protection</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Corporate finance advisory for real estate projects</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Investment consulting</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Tax</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Wealth protection</a></li>
                     </ul>
                 </div>
                 <img src="/img/temp/service-5.webp?v-2" alt="Service image" class="img services-row-image">
@@ -44,8 +44,8 @@
                 <div class="services-row-body">
                     <h3 class="services-row-title">Legal & Relocation Services</h3>
                     <ul>
-                        <li><a href="#" class="primary-link">Legal structuring</a></li>
-                        <li><a href="#" class="primary-link">Relocation to Switzerland</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Legal structuring</a></li>
+                        <li><a href="{{ route('public.services.article') }}" class="primary-link">Relocation to Switzerland</a></li>
                     </ul>
                 </div>
                 <img src="/img/temp/service-3.webp?v-2" alt="Service image" class="img services-row-image">
