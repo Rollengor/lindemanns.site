2468c42bbc850b92c9ce1f6dbed7c2ffe0feacb4
Alexander Silka <alexander.silka@gmail.com>
2025-12-18 17:40:53 +0200

    update pages

---
+++ database/migrations/2025_12_18_152812_create_services_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+
+            $table->json('title');
+            $table->string('slug')->unique();
+            $table->json('short_description')->nullable();
+            $table->json('description');
+
+            $table->json('seo_title')->nullable();
+            $table->json('seo_description')->nullable();
+            $table->json('seo_keywords')->nullable();
+
+            $table->json('services');
+            $table->json('links');
+
+            $table->unsignedInteger('sort')->default(10000);
+            $table->boolean('active')->default(true);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
