103948386c298cdef41c095d44bedcd269c20b4e
Rollengor <alexander.silka@gmail.com>
2025-12-21 14:03:28 +0200

    update hedaer css and js

--- resources/js/public/scroll-page.js
+++ resources/js/public/scroll-page.js
@@ -1,9 +1,31 @@
-export function scrollPage() {
-    document.addEventListener('DOMContentLoaded', toggleScrollClass);
+let lastScroll = 0;
+let lastDownScroll = 0;
+let isScrollDown = false;
 
-    window.addEventListener('scroll', toggleScrollClass);
+export function scrollPage() {
+    document.addEventListener('DOMContentLoaded', toggleScrolledClass);
+
+    window.addEventListener('scroll', () => {
+        const currentScroll = window.scrollY;
+        const windowHeight = window.innerHeight;
+
+        isScrollDown = currentScroll > lastScroll;
+
+        let isHeaderHide = isScrollDown && currentScroll > windowHeight;
+
+        if (isScrollDown) {
+            lastDownScroll = currentScroll;
+        } else {
+            isHeaderHide = !(lastDownScroll - currentScroll > windowHeight / 3 || currentScroll < windowHeight);
+        }
+
+        lastScroll = currentScroll;
+        
+        document.documentElement.classList.toggle('is-header-hide', isHeaderHide);
+        toggleScrolledClass();
+    });
 }
 
-function toggleScrollClass() {
+function toggleScrolledClass() {
     document.documentElement.classList.toggle('is-page-scrolled', window.scrollY > 0/*window.innerHeight*/);
 }
